4306a5ad2892d75e663a7a8abc87596d4532fefe
Wenxuan He <wenxh15@vt.edu>
2019-08-26 16:10:07 -0700

    Updates

---
+++ precache-manifest.1146ca9a29c542472bc505226368af9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "870060eb6e1fe2c04720a97a0f065490",
+    "url": "/simpleTodoList/index.html"
+  },
+  {
+    "revision": "8a24a0daeb55ec089836",
+    "url": "/simpleTodoList/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ffda732652382a63398d",
+    "url": "/simpleTodoList/static/js/2.777547e5.chunk.js"
+  },
+  {
+    "revision": "8a24a0daeb55ec089836",
+    "url": "/simpleTodoList/static/js/main.b4f9ce3a.chunk.js"
+  },
+  {
+    "revision": "3c3c8cb221c0ef3345e0",
+    "url": "/simpleTodoList/static/js/runtime~main.2286086d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simpleTodoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
